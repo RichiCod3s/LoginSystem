e400335c3d4deed88190c0bc18a431e984633936
RichiCod3s <aaronrichardsonsg@gmail.com>
2023-12-08 06:39:30 +0000

    Added Buttons to JFrame

--- src/LoginPage.java
+++ src/LoginPage.java
@@ -11,13 +11,14 @@ import javax.swing.JTextField;
 
 public class LoginPage implements ActionListener{
 
-	JFrame frame = new JFrame();
-	JButton loginButton = new JButton("Login");
-	JButton resetButton = new JButton("Reset");
-	JTextField userIDField = new JTextField();
-	JPasswordField userPasswordField = new JPasswordField(); // like text field but does not show the characters typed
+	JFrame frame = new JFrame(); // create frame
 	
-	JLabel userIDLabel = new JLabel("user ID:" );
+	// create buttons, fields & labels
+	JButton loginButton = new JButton("Login"); // button
+	JButton resetButton = new JButton("Reset");
+	JTextField userIDField = new JTextField(); // where a user types
+	JPasswordField userPasswordField = new JPasswordField(); // like text field but does not show the characters typed
+	JLabel userIDLabel = new JLabel("user ID:" ); // text
 	JLabel userPasswordLabel = new JLabel("password:" );		
 	JLabel messageLabel = new JLabel("");				
 			
@@ -28,9 +29,7 @@ public class LoginPage implements ActionListener{
 	
 		logininfo = loginInfoOriginal;
 		
-		//add fields - where user types
-		userIDField.setBounds(125, 100, 200, 25);
-		userPasswordField.setBounds(125, 150, 200, 25);
+		
 		
 		// label placement: x coordinate, y coordinate, width, height
 		userIDLabel.setBounds(50,100,75,25);
@@ -38,13 +37,29 @@ public class LoginPage implements ActionListener{
 		messageLabel.setBounds(125,250,250,35);
 		messageLabel.setFont(new Font(null, Font.ITALIC,25));
 		
-		// create frame - add components of labels and text to frame
+		//field placement coordinate - where user types
+		userIDField.setBounds(125, 100, 200, 25);
+		userPasswordField.setBounds(125, 150, 200, 25);
+		
+		// button placement coordinates - *need to add action listener to buttons*
+		loginButton.setBounds(125, 200, 100, 25);
+		loginButton.addActionListener(this); 
+		loginButton.setFocusable(false); // Esthetics - this stops a square from appearing around the button text - not needed
+		resetButton.setBounds(225, 200, 100, 25);
+		resetButton.addActionListener(this); 
+		resetButton.setFocusable(true); // Esthetics - this stops a square from appearing around the button text - not needed
+		
+		// create frame - add components of labels, textfields & buttons to frame
 		frame.add(userIDLabel);
 		frame.add(userPasswordLabel);
 		frame.add(messageLabel);
 		
 		frame.add(userIDField);
 		frame.add(userPasswordField);
+		
+		frame.add(loginButton);
+		frame.add(resetButton);
+		
 		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // exits JFrame when closed
 		frame.setSize(420,420); // size of frame
