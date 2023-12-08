ee3e2b7a439ca91266ecabb5b72e5e3032c1e2e8
RichiCod3s <aaronrichardsonsg@gmail.com>
2023-12-08 06:21:39 +0000

    Added fields to JFrame

--- src/LoginPage.java
+++ src/LoginPage.java
@@ -1,3 +1,4 @@
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.HashMap;
@@ -18,7 +19,7 @@ public class LoginPage implements ActionListener{
 	
 	JLabel userIDLabel = new JLabel("user ID:" );
 	JLabel userPasswordLabel = new JLabel("password:" );		
-	JLabel userMessegeLabel = new JLabel();				
+	JLabel messageLabel = new JLabel("");				
 			
 	HashMap<String, String> logininfo = new HashMap<String,String>(); 
 	
@@ -27,14 +28,24 @@ public class LoginPage implements ActionListener{
 	
 		logininfo = loginInfoOriginal;
 		
-		// label placement x coordinate, y coordinate, width, height
+		//add fields - where user types
+		userIDField.setBounds(125, 100, 200, 25);
+		userPasswordField.setBounds(125, 150, 200, 25);
+		
+		// label placement: x coordinate, y coordinate, width, height
 		userIDLabel.setBounds(50,100,75,25);
 		userPasswordLabel.setBounds(50,150,75,25);
-
-		// create frame
+		messageLabel.setBounds(125,250,250,35);
+		messageLabel.setFont(new Font(null, Font.ITALIC,25));
+		
+		// create frame - add components of labels and text to frame
 		frame.add(userIDLabel);
 		frame.add(userPasswordLabel);
-
+		frame.add(messageLabel);
+		
+		frame.add(userIDField);
+		frame.add(userPasswordField);
+		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // exits JFrame when closed
 		frame.setSize(420,420); // size of frame
 		frame.setLayout(null); // no layout manager
