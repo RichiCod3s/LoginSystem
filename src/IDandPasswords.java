df27d7907186dce183a37b4761fb3b062e7c173c
RichiCod3s <aaronrichardsonsg@gmail.com>
2023-12-08 05:44:58 +0000

    Restart New Commit

--- src/IDandPasswords.java
+++ src/IDandPasswords.java
@@ -2,14 +2,19 @@ import java.util.HashMap;
 
 public class IDandPasswords {
 
-	HashMap<String,String> logininfo = new HashMap<String,String>(); // HashMap is good as it has key/value pairs
+	HashMap<String, String> logininfo = new HashMap<String, String>();
 	
 	IDandPasswords(){
 		
-		logininfo.put("Richi","Pizza"); // put is a method in Java's HashMap class that is used to insert a key-value pair into the map.
-		logininfo.put("RichiCod3s","PASSWORD");
-		logininfo.put("RichiRich","abc123");
+		logininfo.put("Bro","pizza");
+		logininfo.put("Richie","Rich");
+		logininfo.put("BroCode","abc123");
+	}
+	
+	protected HashMap getLoginInfo(){
+		return logininfo;
 		
 	}
-
+	
+	
 }
--- src/LoginPage.java
+++ src/LoginPage.java
@@ -1,4 +1,50 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
 
-public class LoginPage {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 
+public class LoginPage implements ActionListener{
+
+	JFrame frame = new JFrame();
+	JButton loginButton = new JButton("Login");
+	JButton resetButton = new JButton("Reset");
+	JTextField userIDField = new JTextField();
+	JPasswordField userPasswordField = new JPasswordField(); // like text field but does not show the characters typed
+	
+	JLabel userIDLabel = new JLabel("user ID:" );
+	JLabel userPasswordLabel = new JLabel("password:" );		
+	JLabel userMessegeLabel = new JLabel();				
+			
+	HashMap<String, String> logininfo = new HashMap<String,String>(); 
+	
+	//constructor
+	LoginPage(HashMap<String,String> loginInfoOriginal){
+	
+		logininfo = loginInfoOriginal;
+		
+		// label placement x coordinate, y coordinate, width, height
+		userIDLabel.setBounds(50,100,75,25);
+		userPasswordLabel.setBounds(50,150,75,25);
+
+		// create frame
+		frame.add(userIDLabel);
+		frame.add(userPasswordLabel);
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // exits JFrame when closed
+		frame.setSize(420,420); // size of frame
+		frame.setLayout(null); // no layout manager
+		frame.setVisible(true); // I can see the frame 
+	}
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 }
